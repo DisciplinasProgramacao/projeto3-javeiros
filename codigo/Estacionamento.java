b71168b0b28c77e5cfc8173e6cb3ef825d7c3611
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-10-04 12:20:54 -0300

    Inserindo parte 1 Estacionamento.java #2

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,7 +1,6 @@
 
-
 public class Estacionamento {
-
+    private int contClientes = 1;
 	private String nome;
 	private Cliente[] id;
 	private Vaga[] vagas;
@@ -9,43 +8,65 @@ public class Estacionamento {
 	private int vagasPorFileira;
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
+         this.nome = nome;
+		 this.quantFileiras = fileiras;
+		 this.vagasPorFileira = vagasPorFila;
+		 gerarVagas();
 	}
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
-		
+		for(Cliente c : id){
+				if(idCLi.equals(c.getId)){
+                c.addVeiculo(veiculo);
+				}
+		}
 	}
 
 	public void addCliente(Cliente cliente) {
-		
+		for(int i=0; i < contClientes;i++){
+			id[i] = cliente;
+		}
 	}
 
 	private void gerarVagas() {
-		
+		int tam = quantFileiras * vagasPorFileira;
+        vagas = new Vaga[tam];
 	}
 
 	public void estacionar(String placa) {
-		
+
 	}
 
 	public double sair(String placa) {
-		
+
 	}
 
 	public double totalArrecadado() {
-		
+
 	}
 
 	public double arrecadacaoNoMes(int mes) {
-		
+
 	}
 
 	public double valorMedioPorUso() {
-		
+		double resposta;
+		for(Cliente c : id){
+			resposta = c.arrecadadoTotal()/c.totalDeUsos();
+		}
+		return resposta;
 	}
 
 	public String top5Clientes(int mes) {
-		
+        double valorPrimario = id[0].arrecadacaoNoMes(mes);
+		String topClientes[] = new String[5];
+		for(Cliente c : id){
+			resposta = c.arrecadadoNoMes(mes);
+			if( resposta > valorPrimario ){
+				
+			}
+		}
+
 	}
 
 }
