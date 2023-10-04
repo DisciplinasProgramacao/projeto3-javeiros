8b5d65802dd2cf4ba4b47dffeabd6c9d4948fd7e
aalice-Rodrigues <alice.oliveira@sga.pucminas.br>
2023-10-04 12:00:32 -0300

    Atualização da classe Vaga

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -5,20 +5,31 @@ public class Vaga {
 	private String id;
 	private boolean disponivel;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
 	public Vaga(int fila, int numero) {
-		
+	
 	}
 
-	public boolean estacionar() {
-		
+	public boolean estacionar() throws Exception {
+		throw new Exception("Ainda não implementado");
 	}
 
-	public boolean sair() {
-		
+	public boolean sair() throws Exception {
+		throw new Exception("Ainda não implementado");
 	}
-
-	public boolean disponivel() {
-		
+	public boolean Disponivel() {
+		return disponivel;
 	}
 
 }
