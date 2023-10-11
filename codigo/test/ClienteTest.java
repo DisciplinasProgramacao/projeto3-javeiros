cf0247b5c48876439f5224408c97391ea972431f
Kimberly Spencer <kimberly.liz@sga.pucminas.br>
2023-10-11 11:35:25 -0300

    Importando pacote para testes classe Cliente #13

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.util.LinkedList;
 
