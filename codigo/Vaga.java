5c8bd0906a9ae9c16975c97af9fe1bf12d45a355
Talisson Augusto <126222867+Nisuk3@users.noreply.github.com>
2023-10-11 01:18:06 -0300

    Modificação da Classe Vaga

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -5,20 +5,32 @@ public class Vaga {
 	private String id;
 	private boolean disponivel;
 
-	public Vaga(int fila, int numero) {
-		
-	}
+    public Vaga(char fila, int numero) {
+        this.id = Character.toString(fila).toUpperCase() + String.format("%02d", numero);
+        this.disponivel = true;
+    }
 
-	public boolean estacionar() {
-		
-	}
+    public boolean estacionar() {
+        if (disponivel) {
+            disponivel = false;
+            return true;
+        }
+        return false;
+    }
 
-	public boolean sair() {
-		
-	}
+    public boolean sair() {
+        if (!disponivel) {
+            disponivel = true;
+            return true;
+        }
+        return false;
+    }
 
-	public boolean disponivel() {
-		
-	}
+    public boolean disponivel() {
+        return disponivel;
+    }
 
+	public String toString() {
+        return id;
+    }
 }
