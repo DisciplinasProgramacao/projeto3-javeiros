064876a9e032c3474969d90488aff211329aff91
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-10-11 12:15:50 -0300

    Terminando estacionamento.java #9

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,6 +1,7 @@
+import java.util.Arrays;
 
 public class Estacionamento {
-    private int contClientes = 1;
+	private int contClientes = 1;
 	private String nome;
 	private Cliente[] id;
 	private Vaga[] vagas;
@@ -8,29 +9,29 @@ public class Estacionamento {
 	private int vagasPorFileira;
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-         this.nome = nome;
-		 this.quantFileiras = fileiras;
-		 this.vagasPorFileira = vagasPorFila;
-		 gerarVagas();
+		this.nome = nome;
+		this.quantFileiras = fileiras;
+		this.vagasPorFileira = vagasPorFila;
+		gerarVagas();
 	}
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
-		for(Cliente c : id){
-				if(idCli.equals(c.getId())){
-                c.addVeiculo(veiculo);
-				}
+		for (Cliente c : id) {
+			if (idCli.equals(c.getId())) {
+				c.addVeiculo(veiculo);
+			}
 		}
 	}
 
 	public void addCliente(Cliente cliente) {
-		for(int i=0; i < contClientes;i++){
+		for (int i = 0; i < contClientes; i++) {
 			id[i] = cliente;
 		}
 	}
 
 	private void gerarVagas() {
 		int tam = quantFileiras * vagasPorFileira;
-        vagas = new Vaga[tam];
+		vagas = new Vaga[tam];
 	}
 
 	public void estacionar(String placa) {
@@ -51,25 +52,32 @@ public class Estacionamento {
 
 	public double valorMedioPorUso() {
 		double resposta = 0.0;
-		for(Cliente c : id){
-			resposta = c.arrecadadoTotal()/(double)c.totalDeUsos();
+		for (Cliente c : id) {
+			resposta = c.arrecadadoTotal() / (double) c.totalDeUsos();
 		}
 		return resposta;
 	}
 
 	public String top5Clientes(int mes) {
-        double valorPrimario = id[0].arrecadacaoNoMes(mes);
-		String topClientes[] = new String[5];
-		double resposta = 0.0;
-		for(Cliente c : id){
-			resposta = c.arrecadadoNoMes(mes);
-			if( resposta < valorPrimario ){
-				double aux = resposta;
-				resposta = valorPrimario;
-				valorPrimario = aux;
-			}
-		}
 
-	}
+		double maiorValor = 0.0;
+		String topClientes[] = new String[5];
+
+		for (Cliente c : id) {
+				double valorDoCliente = c.arrecadadoNoMes(mes);
+            if(valorDoCliente > maiorValor){
+				for (int i = 0; i < topClientes.length; i++) {
+                    if(i < topClientes.length -1)
+					{
+					 topClientes[i + 1] = topClientes[i];
+                }
+                topClientes[i] = c.getNome();
+            }
+            maiorValor = valorDoCliente;
+        }
+    }
+
+    return Arrays.toString(topClientes);
+}
 
 }
