409777c221993f5a85e4910a56df7fde2aa3e34d
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-10-17 13:14:17 -0300

    [Test] inserindo classe VagaTest #18

---
+++ codigo/test/VagaTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import codigo.Vaga;
+import codigo.Veiculo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue; 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VagaTest {
+
+    Veiculo carro;
+    Vaga vaga;
+
+    @BeforeAll
+    void setUp(){
+        carro = new Veiculo("QNP-6607");
+        vaga = new Vaga(1,1);
+    }
+    
+    @Test
+    public void testEstacionar(){
+        carro.estacionar(vaga);
+        assertFalse(vaga.estacionar());
+    }
+
+    @Test
+    public void testSair(){
+        carro.sair();
+        assertTrue(vaga.sair());
+    }
+
+    @Test 
+    public void testDisponibilidade(){
+        assertTrue(vaga.Disponivel());
+    }
+}
