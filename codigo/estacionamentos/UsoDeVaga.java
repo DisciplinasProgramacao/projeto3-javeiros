259af35dd6314287e1eee7532ddbba2fe504668a
Kimberly Spencer <kimberly.liz@sga.pucminas.br>
2023-10-23 22:15:38 -0300

    organizando arquivos para sobrecarga de dados

--- codigo/estacionamentos/UsoDeVaga.java
+++ codigo/estacionamentos/UsoDeVaga.java
@@ -110,30 +110,4 @@ public class UsoDeVaga {
     public double getValorPago() {
         return valorPago;
     }
-=======
-
-
-public class UsoDeVaga {
-
-	private static final double FRACAO_USO = 0.25;
-	private static final double VALOR_FRACAO = 4.0;
-	private static final double VALOR_MAXIMO = 50.0;
-	private Vaga vaga;
-	private LocalDateTime entrada;
-	private LocalDateTime saida;
-	private double valorPago;
-
-	public UsoDeVaga(Vaga vaga) {
-		
-	}
-
-	public double sair() {
-		
-	}
-
-	public double valorPago() {
-		
-	}
-
->>>>>>> feature-pedro
 }
