50ef0b96bfe97bdb8673cde5e2d0597375a7c861
Talisson Augusto <126222867+Nisuk3@users.noreply.github.com>
2023-11-25 13:15:53 -0300

    Criação do Enum Turno

---
+++ codigo/estacionamentos/Enums/Turno.java
@@ -0,0 +1,28 @@
+package estacionamentos.Enums;
+import java.time.LocalTime;
+
+public enum Turno {
+    MANHA(LocalTime.of(8, 0), LocalTime.of(12, 0)),
+    TARDE(LocalTime.of(12, 1), LocalTime.of(18, 0)),
+    NOITE(LocalTime.of(18, 1), LocalTime.of(23, 59));
+
+    private final LocalTime inicio;
+    private final LocalTime fim;
+
+    Turno(LocalTime inicio, LocalTime fim) {
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public LocalTime getInicio() {
+        return inicio;
+    }
+
+    public LocalTime getFim() {
+        return fim;
+    }
+
+    public boolean estaNoTurno(LocalTime hora) {
+        return !hora.isBefore(inicio) && !hora.isAfter(fim);
+    }
+}
